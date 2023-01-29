95e61eb10fb1f74083ad32901077a71199299d30
Assis-Souz4 <assissouza-dev@hotmail.com>
2023-01-29 18:52:51 -0300

    manipulação de dados incremento e decremento de atributos da build

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-//braços >>>> buscando as variaveis - começando com somar pq o input começa com zero
-const somar = document.querySelector('#somar')
-const subtrair = document.querySelector('#subtrair')
-const braco = document.querySelector('#braco')
-
-somar.addEventListener('click', (evento) => {
-    braco.value = Number(braco.value) +1
-})
-subtrair.addEventListener('click', (evento) => {
-    braco.value = Number(braco.value) -1
-})
+//braços >>>> buscando as variaveis para incremento e decremento - começando com somar pq o input começa com zero
+const somar = document.querySelector("#somar");
+const subtrair = document.querySelector("#subtrair");
+const braco = document.querySelector("#braco");
+//criando evento para somar atributos
+somar.addEventListener("click", () => {manipulaDados('somar')});
+//criando evento para subtrair atributos
+subtrair.addEventListener("click", () => {manipulaDados('subtrair')});
+//função para manupulação de dados dos atributos
+function manipulaDados(operacao) {
+  if (operacao === "somar") {
+    braco.value = Number(braco.value) + 1;
+  } else {
+    braco.value = Number(braco.value) - 1;
+  }
+}
