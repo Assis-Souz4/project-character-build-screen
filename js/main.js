1d25539436fa881ed3ff258050c4ff0db5b67fa6
Assis-Souz4 <assissouza-dev@hotmail.com>
2023-01-29 15:45:58 -0300

    exemplo de arrow function

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const robotron =document.querySelector('#robotron')
+
+robotron.addEventListener('click', (dizOi)=>{
+    console.log('oi')
+})
+
+ // function dizOi(){
+//     console.log('oi')
+ // }
